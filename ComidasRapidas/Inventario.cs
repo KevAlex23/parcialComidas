9e736cb3daa8093bdad48c43e22813f60986e760
KevAlex19 <kacalvo@unicesar.edu.co>
2020-10-05 19:32:56 -0500

    segundo commit pruebas de ingreso de productos

---
+++ ComidasRapidas/Inventario.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComidasRapidas
+{
+    public class Inventario
+    {
+        public List<ProductoSimple> Bodega = new List<ProductoSimple>();
+
+        public Inventario()
+        {
+        }
+
+
+
+        public string AgregarProductosNuevos(ProductoSimple product)
+        {
+            string _result = "";
+            try
+            {
+                if (product.Cantidad <= 0)
+                    return "La cantidad de prodcuto no es validad, verifique.";
+
+
+                if (AgregarProductos(product.ID, product.Cantidad) == "No hay productos con ese id")
+                {
+                    Bodega.Add(product);
+                }
+                _result = $"El producto " + Bodega[Bodega.Count-1].Nombre + $" con el precio de: ${Bodega[Bodega.Count -1].Precio:n2} y la cantidad de: "+ Bodega[Bodega.Count-1].Cantidad;
+            }
+            catch (Exception e)
+            {
+                _result = e.Message;
+            }
+
+            return _result;
+        }
+
+        public string AgregarProductos(string id, int cantidad)
+        {
+            string _result = "";
+            try
+            {
+                var _position = Bodega.FindIndex(val => val.ID == id);
+                if (_position == -1)
+                    return "El producto solicitado no existe";
+                int cantidadAnterior = Bodega[_position].Cantidad;
+                Bodega[_position].Cantidad += cantidad;
+                _result = "Se actualizo el producto: " + Bodega[_position].Nombre + " cantidad anterior: " + cantidadAnterior+", Cantidad actual: "+ Bodega[_position].Cantidad;
+            }
+            catch (Exception e)
+            {
+                _result = e.Message;
+            }
+            return _result;
+        }
+
+        public string QuitarProductos(string id, int cantidad)
+        {
+            string _result = "";
+
+            try
+            {
+                foreach (var item in Bodega)
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            if (Bodega.Find(val => val.ID == id) == null)
+                return "El producto solicitado no existe";
+
+            Bodega.Find(val => val.ID == id);
+            return _result;
+        }
+
+
+    }
+}
--- ComidasRapidasTests/InvetarioTests.cs
+++ ComidasRapidasTests/InvetarioTests.cs
@@ -25,7 +25,7 @@ namespace ComidasRapidas.Tests
             //Preparar
             ProductoSimple producto = new ProductoSimple(id: "2", nombre: "pan de perro", precio: 0, costo: 1000, cantidad: 0);
             //Acción
-            var inventario = new Invetario().AgregarProductos(producto);
+            var inventario = new Inventario().AgregarProductosNuevos(producto);
             //Verificación
             Assert.AreEqual("La cantidad de prodcuto no es validad, verifique.", inventario);
         }
@@ -36,24 +36,63 @@ namespace ComidasRapidas.Tests
         //1.2 La cantidad de la entrada se le aumentará a la cantidad existente del producto
         //Ejemplo
         //Dado Un producto cuya cantidad es mayor a 0 se debe agregar al inventario        //A =>Arrange /Preparación
-        // id: "2", nombre: "pan de perro", precio: 0, costo: 1000, cantidad: 0                              
-        //Cuando Va a consignar un valor menor o igual a cero  (0)                            //A =>Act = Acción
-        //Entonces El sistema presentará el mensaje. “El valor a consignar es incorrecto”  //A => Assert => Validación
+        // id: "2", nombre: "pan de perro", precio: 0, costo: 1000, cantidad: 3                              
+        //Cuando Va a agregar un producto con cantidad menor o igual a cero  (0)                            //A =>Act = Acción
+        //Entonces El sistema presentará el mensaje. "El producto pan de perro con el precio de: $0.00 y la cantidad de: 3"  //A => Assert => Validación
         [TestMethod()]
         public void AgregarProductosATest()
         {
             //Preparar
             ProductoSimple producto = new ProductoSimple(id: "2", nombre: "pan de perro", precio: 0, costo: 1000, cantidad: 3);
             //Acción
-            var inventario = new Invetario().AgregarProductos(producto);
+            var inventario = new Inventario().AgregarProductosNuevos(producto);
             //Verificación
-            Assert.AreEqual("El producto pan de perro con el precio de: $0.00 y la cantidad de: 1", inventario);
+            Assert.AreEqual("El producto pan de perro con el precio de: $0.00 y la cantidad de: 3", inventario);
         }
 
+        //Escenario: Cantidad del producto negativo o cero 
+        //H1: COMO USUARIO QUIERO REGISTRAR LA ENTRADA PRODUCTOS  
+        //Criterio de Aceptación:
+        //1.2 La cantidad de la entrada se le aumentará a la cantidad existente del producto
+        //Ejemplo
+        //Dado Un producto cuya cantidad es mayor a 0 se debe agregar al inventario        //A =>Arrange /Preparación
+        // id: "2", nombre: "pan de perro", precio: 0, costo: 1000, cantidad: 3                             
+        //Cuando Va a agregar cantidad de producto a uno ya registrado se le suma al inventario actual                          //A =>Act = Acción
+        //Entonces El sistema presentará el mensaje. "Se actualizo el producto: pan de perro cantidad anterior: 3, Cantidad actual: 23" //A => Assert => Validación
         [TestMethod()]
-        public void QuitarProductosTest()
+        public void AgregarCantidadProductosActualesTest()
         {
-            Assert.Fail();
+            //Preparar
+            Inventario alamacen = new Inventario();
+            ProductoSimple producto = new ProductoSimple(id: "2", nombre: "pan de perro", precio: 0, costo: 1000, cantidad: 3);
+            //Acción
+            alamacen.AgregarProductosNuevos(producto);
+            var inventario = alamacen.AgregarProductos("2",20);
+            //Verificación
+            Assert.AreEqual("Se actualizo el producto: pan de perro cantidad anterior: 3, Cantidad actual: 23", inventario);
         }
+
+        //Escenario: Cantidad del producto negativo o cero 
+        //H1: COMO USUARIO QUIERO REGISTRAR LA ENTRADA PRODUCTOS  
+        //Criterio de Aceptación:
+        //1.2 La cantidad de la entrada se le aumentará a la cantidad existente del producto
+        //Ejemplo
+        //Dado Un producto cuya cantidad es mayor a 0 se debe agregar al inventario        //A =>Arrange /Preparación
+        // id: "2", nombre: "pan de perro", precio: 0, costo: 1000, cantidad: 3                             
+        //Cuando Va a agregar cantidad de producto a uno que no ha sido registrado se le suma al inventario actual                          //A =>Act = Acción
+        //Entonces El sistema presentará el mensaje. "Se actualizo el producto: pan de perro cantidad anterior: 3, Cantidad actual: 23" //A => Assert => Validación
+        [TestMethod()]
+        public void AgregarCantidadProductosActualeFallaTest()
+        {
+            //Preparar
+            Inventario alamacen = new Inventario();
+            ProductoSimple producto = new ProductoSimple(id: "2", nombre: "pan de perro", precio: 0, costo: 1000, cantidad: 3);
+            //Acción
+            alamacen.AgregarProductosNuevos(producto);
+            var inventario = alamacen.AgregarProductos("1", 20);
+            //Verificación
+            Assert.AreEqual("El producto solicitado no existe", inventario);
+        }
+
     }
 }
